a323a20c76e0086cc4b2d7a08e9c844b424c5f75
Redoni <rk64888@ubt-uni.net>
2024-01-13 20:53:51 +0100

    commit 23

---
+++ AboutUs.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Projekti</title>
+  <link rel="stylesheet" href="Slider.css">
+</head>
+<body>
+  <header>
+    <div class="header-left">
+      <div id="company-name">Rent a Car - RINAS</div>
+    </div>
+    <nav>
+      <ul>
+        <li><a href="Projekti.php">Home</a></li>
+        <li><a href="Slider.php">Products</a></li>
+        <li><a href="Prices.php">Prices</a></li>
+      </ul>
+    </nav>
+    <div class="header-right">
+      <div class="buttons">
+        <a href="LogInForm.php"><button id="login-button">Log In / Sign Up</button></a>
+      </div>
+    </div>
+  </header>
+  
+</body>
+</html>
